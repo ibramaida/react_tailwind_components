9705298108337ae36b4ea007c29bf1790e158375
ibramaida <bmpcbob@gmail.com>
2025-03-21 23:53:25 +0100

    added some features to navbar hover

--- src/components/navbars/NavbarHover/NavbarHover.jsx
+++ src/components/navbars/NavbarHover/NavbarHover.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 const items = [
   {
@@ -17,10 +17,9 @@ const items = [
 
 const Link = ({ item, activeItem, onHover }) => {
   const linkRef = useRef();
+
   const handleHover = () => {
     const rect = linkRef.current.getBoundingClientRect();
-    console.log(rect);
-
     onHover(item, `${rect.x}px`);
   };
 
@@ -28,7 +27,7 @@ const Link = ({ item, activeItem, onHover }) => {
     <a
       ref={linkRef}
       onMouseEnter={handleHover}
-      className={item?.name === activeItem?.name ? "active" : ""}
+      className={`p-4 ${item?.name === activeItem?.name ? "active" : ""}`}
     >
       {item.name}
     </a>
@@ -47,12 +46,28 @@ const Search = () => {
 export const NavbarHover = () => {
   const [activeItem, setActiveItem] = useState(null);
   const [translateX, setTranslateX] = useState("0");
+  const dropdownRef = useRef();
+
   const handleLinkHover = (item, x) => {
     setActiveItem(item || null);
     setTranslateX(x);
   };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setActiveItem(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <nav className="bg-white p-4 flex flex-col md:flex-row md:items-center md:gap-8">
+    <nav className="fixed top-0 left-0 w-full bg-white px-4 flex flex-col md:flex-row md:items-center md:gap-8">
       <img
         className="size-10 rounded-full"
         src="https://randomuser.me/api/portraits/women/3.jpg"
@@ -68,13 +83,18 @@ export const NavbarHover = () => {
           />
         ))}
         <div
+          ref={dropdownRef}
           style={{
             translate: `${translateX} 0`,
           }}
-          className={`${activeItem ? "visible" : ""}`}
+          className={`fixed top-16 left-0 z-10 py-4 rounded-lg grid gap-2 bg-slate-200 ${
+            activeItem ? "visible opacity-100 h-max" : "opacity-0 invisible h-0"
+          } overflow-hidden`}
         >
           {activeItem?.items?.map((link, index) => (
-            <a key={index}>{link}</a>
+            <a className="min-w-28 px-4" key={index}>
+              {link}
+            </a>
           ))}
         </div>
       </div>
